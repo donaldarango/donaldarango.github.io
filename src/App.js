aba83de699d0f4e1d8623c1642b672a8387dfd2d
Donald Arango <92880942+donaldarango@users.noreply.github.com>
2026-06-05 02:06:50 -0400

    swapped router to HasRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
